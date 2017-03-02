2a5e874892ae6a1a8c46713e71eab3f4842777a0
Marcus Longmuir <marcuslongmuir@me.com>
2017-03-02 11:44:03 +0000

    Removed export usage

--- src/js/iterateHeaders.js
+++ src/js/iterateHeaders.js
@@ -1,5 +1,5 @@
 // This function is written in JS (ES5) to avoid an issue with TypeScript targeting ES5, but requiring Symbol.iterator
-export function iterateHeaders(headers, callback) {
+function iterateHeaders(headers, callback) {
   var iterator = headers[Symbol.iterator]();
   var entry = iterator.next();
   while(!entry.done) {
@@ -8,7 +8,7 @@ export function iterateHeaders(headers, callback) {
   }
 }
 
-export function iterateHeadersKeys(headers, callback) {
+function iterateHeadersKeys(headers, callback) {
   var iterator = headers.keys();
   var entry = iterator.next();
   while(!entry.done) {
@@ -16,3 +16,8 @@ export function iterateHeadersKeys(headers, callback) {
     entry = iterator.next();
   }
 }
+
+module.exports = {
+  iterateHeaders,
+  iterateHeadersKeys,
+};
