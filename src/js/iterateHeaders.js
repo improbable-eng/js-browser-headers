a89c8d35838f261ea3b159208183c71c4219904d
Marcus Longmuir <marcuslongmuir@me.com>
2017-03-02 10:34:40 +0000

    Wrote iterateHeaders as ES5

--- src/js/iterateHeaders.js
+++ src/js/iterateHeaders.js
@@ -1,12 +1,18 @@
-// This function is written in JS to avoid an issue with TypeScript targeting ES5 with iterators
+// This function is written in JS (ES5) to avoid an issue with TypeScript targeting ES5, but requiring Symbol.iterator
 export function iterateHeaders(headers, callback) {
-  for (let entry of headers) {
-    callback(entry);
+  var iterator = headers[Symbol.iterator]();
+  var entry = iterator.next();
+  while(!entry.done) {
+    callback(entry.value[0]);
+    entry = iterator.next();
   }
 }
 
 export function iterateHeadersKeys(headers, callback) {
-  for (let key of headers.keys()) {
-    callback(key);
+  var iterator = headers.keys();
+  var entry = iterator.next();
+  while(!entry.done) {
+    callback(entry.value);
+    entry = iterator.next();
   }
 }
