c9676d4ed4531e4be63ad9486977ce7c9a581c39
Marcus Longmuir <marcuslongmuir@me.com>
2017-03-02 10:39:27 +0000

    Make JS ES5 (#7)
    * Make JS ES5
    
    * Moved js to inner directory
    
    * Wrote iterateHeaders as ES5

--- src/BrowserHeaders.ts
+++ src/BrowserHeaders.ts
@@ -1,5 +1,24 @@
-import {normalizeName, normalizeValue, getHeaderValues, getHeaderKeys, splitHeaderValue } from "./util";
+import { normalizeName, normalizeValue, getHeaderValues, getHeaderKeys, splitHeaderValue } from "./util";
 import { WindowHeaders } from "./WindowHeaders";
+
+/** @internal */
+export interface Map<K, V> {
+  clear(): void;
+  delete(key: K): boolean;
+  forEach(callbackfn: (value: V, index: K, map: Map<K, V>) => void, thisArg?: any): void;
+  get(key: K): V | undefined;
+  has(key: K): boolean;
+  set(key: K, value?: V): this;
+  readonly size: number;
+}
+
+interface MapConstructor {
+  new (): Map<any, any>;
+  new <K, V>(entries?: [K, V][]): Map<K, V>;
+  readonly prototype: Map<any, any>;
+}
+
+declare const Map: MapConstructor;
 
 export type HeaderObject = {[key: string]: string|string[]};
 export type HeaderMap = Map<string, string|string[]>;
